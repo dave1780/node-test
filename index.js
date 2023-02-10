704be9bf36ef53ecb8fabc7982cb0802c07aec44
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-10 18:21:22 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// run `node index.js` in the terminal
+import * as fs from "fs"
+import http from "http"
+import * as dns from "dns"
+dns.lookup("www.w3schools.com",(err,address,family)=>{
+  console.log(address)
+  console.log(family)
+})
+dns.resolve("www.w3schools.com","A" ,(err,data)=>{
+  console.log(data)
+
+})
+
