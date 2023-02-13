380dc8adb26be03697e3f6baede06676cd04103d
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-13 19:12:51 +0545

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 import * as fs from "fs"
 import http from "http"
 import * as dns from "dns"
-dns.lookup("www.w3schools.com",(err,address,family)=>{
-  console.log(address)
-  console.log(family)
+import express from "express"
+const server = express();
+server.get('/',(req,res)=>{
+  res.send("you are listening to port 3000")
 })
-dns.resolve("www.w3schools.com","A" ,(err,data)=>{
-  console.log(data)
-
+server.listen(3000,()=>{
+  console.log("listening to port 3000")
 })
-console.log("express ")
