0aed5540d9f2e79b6510ce02ea962816bf30b5cc
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-17 18:21:13 +0545

    1 file modified, 1 deleted

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 // run `node index.js` in the terminal
-import * as fs from "fs"
-import http from "http"
-import * as dns from "dns"
-import express from "express"
-const server = express();
-server.get('/',(req,res)=>{
-  res.send("you are listening to port 3000")
-})
-server.listen(3000,()=>{
-  console.log("listening to port 3000")
-})
+import Nav from "./Nav.js"
+Nav.getData
